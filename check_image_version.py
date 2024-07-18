cc24d2f698a8834f88886d3204c21e08bb8862ab
dupeng <dunett@163.com>
2024-07-19 00:04:16 +0800

    注释测试2

--- check_image_version.py
+++ check_image_version.py
@@ -4,5 +4,6 @@ for tag in image_tags:
     tag = tag.strip()
     source_tag = tag.split(' ')[0]
     target_tag = tag.split(' ')[1]
-    if not source_tag.__contains__(':'):
-        raise Exception(f"No version is specified. Please add a version: {source_tag}")
+    if not source_tag.startswith('#'):
+        if not source_tag.__contains__(':'):
+            raise Exception(f"No version is specified. Please add a version: {source_tag}")
