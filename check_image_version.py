20c88ce82e4256bb93e9cb9411b3481111e2ad45
dupeng <dunett@163.com>
2024-07-18 23:54:40 +0800

    check test

---
+++ check_image_version.py
@@ -0,0 +1,8 @@
+image_tags = open('./to_pull.txt').readlines()
+
+for tag in image_tags:
+    tag = tag.strip()
+    source_tag = tag.split(' ')[0]
+    target_tag = tag.split(' ')[1]
+    if not source_tag.__contains__(':'):
+        raise Exception(f"No version is specified. Please add a version: {source_tag}")
