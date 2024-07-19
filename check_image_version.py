ed9ac95a9ee1e76fb0c0f7492cfed76c94c6567e
dupeng <dunett@163.com>
2024-07-19 10:49:05 +0800

    pg16-2

--- check_image_version.py
+++ check_image_version.py
@@ -2,7 +2,7 @@ image_tags = open('./to_pull.txt').readlines()
 
 for tag in image_tags:
     tag = tag.strip()
-    if len(tag) == 0:
+    if len(tag) == 0 or tag.startswith('#'):
         continue
     source_tag = tag.split(' ')[0]
     target_tag = tag.split(' ')[1]
